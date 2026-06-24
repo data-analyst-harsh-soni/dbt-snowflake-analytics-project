8dd7323ba4bc0a2ca06ec653615d5211409e2ec8
Harsh Soni <harshnewa@gmail.com>
2026-06-24 19:48:16 +0530

    Add files via upload

---
+++ models/staging/jaffle_shop/stg_customers.sql
@@ -0,0 +1,13 @@
+with customers as (
+
+    select
+        id as customer_id,
+        first_name,
+        last_name
+
+    from {{ source('jaffle_shop', 'CUSTOMER') }}
+
+)
+
+select *
+from customers
