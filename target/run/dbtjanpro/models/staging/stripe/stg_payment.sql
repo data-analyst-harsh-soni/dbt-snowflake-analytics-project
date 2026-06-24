30ad969fa099cf7b4b203aaec2b288f7b7737847
Harsh Soni <harshnewa@gmail.com>
2026-06-24 19:59:40 +0530

    Add files via upload

---
+++ target/run/dbtjanpro/models/staging/stripe/stg_payment.sql
@@ -0,0 +1,17 @@
+
+  create or replace   view RAW.JAFFLE_SHOP.stg_payment
+  
+  
+  
+  
+  as (
+    select 
+id as payment,
+orderid as order_id,
+paymentmethod as payment_method,
+status,
+amount / 100 as amount ,
+created as created_as
+from raw.stripe.payment
+  );
+
